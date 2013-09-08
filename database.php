133e3e13baeb4e6bf20c1d27c98c4fd152e07248
Deborah Alves <deborah.alves@gmail.com>
2013-09-07 21:07:48 -0400

    database for live

--- database.php
+++ database.php
@@ -1,13 +1,13 @@
 <?	
-	define("DB_HOST", "localhost:8888");
-	define("DB_USERNAME", "root");
-	define("DB_PASSWORD", "root");
-	define("DB_DATABASE", "pennapps");
+	// define("DB_HOST", "localhost:8888");
+	// define("DB_USERNAME", "root");
+	// define("DB_PASSWORD", "root");
+	// define("DB_DATABASE", "pennapps");
 
-	// define("DB_HOST", "localhost");
-	// define("DB_USERNAME", "deborahh_penn");
-	// define("DB_PASSWORD", "pennapps2013");
-	// define("DB_DATABASE", "deborahh_pennapps");
+	define("DB_HOST", "localhost");
+	define("DB_USERNAME", "deborahh_penn");
+	define("DB_PASSWORD", "pennapps2013");
+	define("DB_DATABASE", "deborahh_pennapps");
 
 	// connects to the database
 	// TODO: handle errors
