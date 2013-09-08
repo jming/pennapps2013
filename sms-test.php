73c437008a92404f3812b513c27ee2c2f9016f24
Alisa <alisan011@gmail.com>
2013-09-08 00:12:39 -0400

    Fixing almost final(?) bugs in SMS SQL connection!!!!!

--- sms-test.php
+++ sms-test.php
@@ -21,12 +21,12 @@
         "report"=>"[1/7] Where are you located? Reply with \"location [location]\". For example, the name of the nearest school. For help, reply with \"?\"",
         "location"=>"[2/7] What symptoms occur? Reply with \"symptoms [space separated list of symptoms]\". Examples of symptoms are chills, fever, headache. For help, reply with \"?\"",
         "symptoms"=>"[3/7] What date did the symptoms begin? Reply with \"date-onset YYYY-MM-DD\" For help, reply with \"?\"",
-        "date-onset"=> "[4a/7] Has the patient been diagnosed with malaria by a health professional? Reply with \"diagnose [yes/no]\" For help, reply with \"?\"",
-        "diagnose"=>"[4b/7] If the patient has been diagnosed, what was the date of the diagnosis? Reply with \"date-diagnosed YYYY-MM-DD\" For help, reply with \"?\"",
+        "date-onset"=> "[4a/7] Has the patient been diagnosed with malaria by a health professional? Reply with \"diagnose [y/n]\" For help, reply with \"?\"",
+        "diagnose"=>"[4b/7] If the patient has been diagnosed, what was the date of the diagnosis? Reply with \"date-diagnosed YYYY-MM-DD\". For help, reply with \"?\"",
         "date-diagnosed"=>"[5/7] What age is the patient? Reply with \"age [age]\" or \"age -\" if you prefer not to answer. For help, reply with \"?\"",
         "age"=>"[6/7] What is the gender of the patient? Reply with \"gender [gender]\" or \"gender -\" if you prefer not to answer. For help, reply with \"?\"",
-        "gender"=>"[7/7] Thank you! The malaria incidence has been recorded. Visit deborahhh.com/index.php to view the Malaria Map.",
-        "?"=>"Please visit deborahhh.com/help for more information.",
+        "gender"=>"[7/7] Thank you! The malaria incidence has been recorded. Visit deborahhh.com/pennapps/index.php to view the Malaria Map.",
+        "?"=>"Please visit deborahhh.com/pennapps/#info for more information.",
     );
  
     // parse body of text message, if form follows
@@ -36,7 +36,11 @@
     $field = strtolower($textArray[0]);
     
     if($response = $smsResponses[$field]) {
-        $report[$field] = strtolower($textArray[1]);
+        if(strcasecmp($field, "symptoms") == 0) {
+            $report[$field] = array_slice($textArray, 1);
+        } else {
+            $report[$field] = strtolower($textArray[1]);
+        }
     }
     
     // save it
@@ -71,16 +75,16 @@
 
         $result = mysql_query("INSERT INTO reports (age, gender, onset, diagnosed, diagdate, latitude, longitude, currentdate) VALUES ($age, '$gender', '$onset', '$diagnosed', '$date', '$latitude', '$longitude', '$curdate')");
         
-        if (!$result):
-            // TODO: display error
-        else:
-            $id = mysql_insert_id();
-            foreach ($symptoms as $symp) {
-                $symp = mysql_real_escape_string($symp);
-                $result = mysql_query("INSERT INTO symptoms (report_id, symptom) VALUES ($id, '$symp')");
-            }
-            // TODO: display success
-        endif;
+		if (!$result):
+			$("#database-failure").popup();
+		else:
+			$id = mysql_insert_id();
+			foreach ($symptoms as $symp) {
+				$symp = mysql_real_escape_string($symp);
+				$result = mysql_query("INSERT INTO symptoms (report_id, symptom) VALUES ($id, '$symp')");
+			}		
+			$("#database-success").popup();
+		endif;
     }
  
     // now respond to text
